25843d428c16cbe30ade9b3794ff569ca6381694
CemCanplt <cem--canpolat@hotmail.com>
2025-04-08 22:47:15 +0300

    Sonunda

--- src/main/java/com/workintech/s17d2/S17d2Application.java
+++ src/main/java/com/workintech/s17d2/S17d2Application.java
@@ -2,8 +2,10 @@ package com.workintech.s17d2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.workintech.s17d2")
 public class S17d2Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/workintech/s17d2/tax/DeveloperTax.java
@@ -0,0 +1,29 @@
+package com.workintech.s17d2.tax;
+
+
+import org.springframework.stereotype.Component;
+
+@Component("developerTax")
+public class DeveloperTax implements Taxable {
+
+    public static final double SIMPLE_TAX_RATE = 15d;
+
+    public static final double MIDDLE_TAX_RATE = 25d;
+
+    public static final double UPPER_TAX_RATE = 35d;
+
+    @Override
+    public Double getSimpleTaxRate() {
+        return DeveloperTax.SIMPLE_TAX_RATE;
+    }
+
+    @Override
+    public Double getMiddleTaxRate() {
+        return DeveloperTax.MIDDLE_TAX_RATE;
+    }
+
+    @Override
+    public Double getUpperTaxRate() {
+        return DeveloperTax.UPPER_TAX_RATE;
+    }
+}
